a97b91cf22f87d5d7ecb37c8fb5545792601d57f
zxielle <@JahazielFaye>
2023-03-13 21:45:18 -0400

    Ui fixed on index.js

--- react-portfolio/pages/_app.js
+++ react-portfolio/pages/_app.js
@@ -1,6 +1,9 @@
+// import the global styles file
 import '../styles/globals.css'
 
+// define the main component of the app
 function MyApp({ Component, pageProps }) {
+    // return the child component with its props
   return <Component {...pageProps} />
 }
 
